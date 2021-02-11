86dabb9a27c7a88c2559ccb5fa6851e18a2bd8b1
Bushra05Rafia <52478317+Bushra05Rafia@users.noreply.github.com>
2021-02-12 02:54:39 +0600

    Tasks

--- Find_Frequency_of_SubString.py
+++ Find_Frequency_of_SubString.py
@@ -4,17 +4,20 @@ def count(str, sub):
     x = len(str)
     while True:
         if counter == True:
-            findsub = ''
+            findsub =''
             for i in sub:
                 for j in range(x):
                     if i == str[j]:
+                        print("i" ,i)
                         findsub += str[j]
+                        print("findsub",findsub)
                         str[j] = ''
                         break
                 if i not in str:
                     counter = False
             if sub == findsub:
                 result = result + 1
+                print("result",result)
         else: break
     return result
 
@@ -23,4 +26,4 @@ if __name__ == '__main__':
     str=list(input())
     print("Enter the SubString: ")
     sub=input()
-    print(count(str,sub))
+    print("Final Count=", count(str,sub))
